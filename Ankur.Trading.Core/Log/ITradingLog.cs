89eaec859dffebd859685e20d63b8dd7c45455da
OnxP <ankurpatel0000@hotmail.com>
2018-09-27 23:31:10 +0100

    added ignored files

---
+++ Ankur.Trading.Core/Log/ITradingLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ankur.Trading.Core.Log
+{
+    public interface ITradingLog
+    {
+        string Pair { get; }
+        decimal Quantity { get;  }
+        decimal BtcQuantity { get;  }
+        decimal Price { get;  }
+        DateTime CloseTime { get;  }
+    }
+}
