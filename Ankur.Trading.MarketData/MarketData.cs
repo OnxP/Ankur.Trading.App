990c3f50c5c08d267315a383bf5a9d547aec9914
OnxP <ankurpatel0000@hotmail.com>
2020-08-18 21:06:25 +0100

    markte data

---
+++ Ankur.Trading.MarketData/MarketData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ankur.Trading.MarketData
+{
+    public class MarketData
+    {
+        //responsible to connect to Binance and load the candlesticks.
+        //need 2 methods to run this, for back testing and for live testing.
+
+
+
+    }
+}
